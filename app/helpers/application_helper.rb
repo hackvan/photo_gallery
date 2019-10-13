a81e45ce4608a90d8910a0778406dfb42e5a9e27
Diego Camacho <hackvan@gmail.com>
2019-10-13 10:16:18 -0500

    Improves titles and header texts depending on the routes.
    	- Creates page_title helper to change the title page.
    	- Creates nav_link_to helper to render nav links with active property.
    	- Creates not_found partial for photos when there are no results.
    	- Sends from PhotosController info about titles and headers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def page_title
+    if content_for?(:title)
+      "IdeaGallery - #{content_for(:title)}"
+    else
+      "IdeaGallery"
+    end
+  end
+
+  def nav_link_to(text, url)
+    classes = ['nav-link']
+    classes << 'active' if current_page?(url)
+    link_to(text, url, class: classes.join(' '))
+  end
 end
